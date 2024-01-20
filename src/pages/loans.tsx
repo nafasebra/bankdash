3f9dd075729e6fca7da51b8475d5f353f4f396d1
Nafas Ebrahimi <ebranafas@gmail.com>
2024-01-20 21:19:15 +0330

    feat: add loans table

--- src/pages/loans.tsx
+++ src/pages/loans.tsx
@@ -2,6 +2,7 @@ import { linkEnum } from '@/enums/link'
 import Body from '@/components/layout/Body'
 import { AccountIcon, BarIcon, Service2nIcon, Settings2Icon } from '@/assets/icons'
 import IconCard from '@/components/ui/card/IconCard'
+import LoanTable from '@/components/ui/table/LoanTable'
 
 function Loans() {
   return (
@@ -11,6 +12,10 @@ function Loans() {
         <IconCard icon={<Service2nIcon />} supTitle='Corporate Loans' title='$100,000' mainColor='text-yellow' mainColorAlpha='bg-yellow/20' />
         <IconCard icon={<BarIcon />} supTitle='Business Loans' title='$500,000' mainColor='text-magenta' mainColorAlpha='bg-magenta/20' />
         <IconCard icon={<Settings2Icon />} supTitle='Custom Loans' title='Choose Money' mainColor='text-cyan' mainColorAlpha='bg-cyan/20' />
+      </div>
+      <div className='flex flex-col gap-5'>
+        <p className='flex items-center text-2xl font-semibold text-primary-200'>Active Loans Overview</p>
+        <LoanTable />
       </div>
     </Body>
   )
