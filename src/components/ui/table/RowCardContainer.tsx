43192c9b289212db2ef7e803b43626d365a386a0
Nafas Ebrahimi <ebranafas@gmail.com>
2024-01-24 13:56:15 +0330

    chore: made row card and the container for table cards

---
+++ src/components/ui/table/RowCardContainer.tsx
@@ -0,0 +1,16 @@
+import RowCard from "../card/RowCard";
+
+function RowCardContainer() {
+  return (
+    <div className="overflow-x-hidden">
+      <div className="overflow-x-auto space-y-3">
+        <RowCard />
+        <RowCard />
+        <RowCard />
+        <RowCard />
+      </div>
+    </div>
+  );
+}
+
+export default RowCardContainer;
--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,8 +1,9 @@
 import { linkEnum } from "@/enums/link";
 import Body from "@/components/layout/Body";
-import { BagIcon, LifeIcon, SafeIcon, SellIcon } from "@/assets/icons";
+import { BagIcon, LifeIcon, SafeIcon } from "@/assets/icons";
 import IconCard from "@/components/ui/card/IconCard";
 import SectionWithTitle from "@/components/pages/SectionWithTitle";
+import RowCardContainer from "@/components/ui/table/RowCardContainer";
 
 function Services() {
   return (
@@ -34,114 +35,7 @@ function Services() {
         />
       </div>
       <SectionWithTitle title="Bank Services List">
-        <div className="overflow-x-hidden">
-          <div className="overflow-x-auto space-y-3">
-            <div className="bg-white rounded-lg flex gap-10 justify-between py-4 px-5">
-              <div className="flex items-center gap-3">
-                <div className={`w-12 h-12 flex items-center justify-center rounded-full bg-magenta/20 text-magenta`}>
-                  <div className="w-6 h-6 flex items-center justify-center">
-                    <SellIcon />
-                  </div>
-                </div>
-                <div className="text-lg flex flex-col whitespace-nowrap">
-                  <p className="text-black leading-0">It is a long established </p>
-                  <p className="text-primary-400 leading-0">Business loans</p>
-                </div>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <a href="#" className="w-[150px] flex items-center justify-center py-2 px-4 rounded-full border-2 border-primary-400 text-primary-400 text-center text-lg">View details</a>
-            </div>
-            <div className="bg-white rounded-lg flex gap-10 justify-between py-4 px-5">
-              <div className="flex items-center gap-3">
-                <div className={`w-12 h-12 flex items-center justify-center rounded-full bg-magenta/20 text-magenta`}>
-                  <div className="w-6 h-6 flex items-center justify-center">
-                    <SellIcon />
-                  </div>
-                </div>
-                <div className="text-lg flex flex-col whitespace-nowrap">
-                  <p className="text-black leading-0">It is a long established </p>
-                  <p className="text-primary-400 leading-0">Business loans</p>
-                </div>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <a href="#" className="w-[150px] flex items-center justify-center py-2 px-4 rounded-full border-2 border-primary-400 text-primary-400 text-center text-lg">View details</a>
-            </div>
-            <div className="bg-white rounded-lg flex gap-10 justify-between py-4 px-5">
-              <div className="flex items-center gap-3">
-                <div className={`w-12 h-12 flex items-center justify-center rounded-full bg-magenta/20 text-magenta`}>
-                  <div className="w-6 h-6 flex items-center justify-center">
-                    <SellIcon />
-                  </div>
-                </div>
-                <div className="text-lg flex flex-col whitespace-nowrap">
-                  <p className="text-black leading-0">It is a long established </p>
-                  <p className="text-primary-400 leading-0">Business loans</p>
-                </div>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <a href="#" className="w-[150px] flex items-center justify-center py-2 px-4 rounded-full border-2 border-primary-400 text-primary-400 text-center text-lg">View details</a>
-            </div>
-            <div className="bg-white rounded-lg flex gap-10 justify-between py-4 px-5">
-              <div className="flex items-center gap-3">
-                <div className={`w-12 h-12 flex items-center justify-center rounded-full bg-magenta/20 text-magenta`}>
-                  <div className="w-6 h-6 flex items-center justify-center">
-                    <SellIcon />
-                  </div>
-                </div>
-                <div className="text-lg flex flex-col whitespace-nowrap">
-                  <p className="text-black leading-0">It is a long established </p>
-                  <p className="text-primary-400 leading-0">Business loans</p>
-                </div>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <div className="text-lg flex flex-col whitespace-nowrap">
-                <p className="text-black leading-0">It is a long established </p>
-                <p className="text-primary-400 leading-0">Business loans</p>
-              </div>
-              <a href="#" className="w-[150px] flex items-center justify-center py-2 px-4 rounded-full border-2 border-primary-400 text-primary-400 text-center text-lg">View details</a>
-            </div>
-          </div>
-        </div>
+        <RowCardContainer />
       </SectionWithTitle>
     </Body>
   );
